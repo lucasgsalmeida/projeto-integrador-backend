c82c4ad4cc364b31eee28024bfc2a35c35a2fda9
Lucas Gabriel <lucasgsalmeida21@gmail.com>
2024-08-18 15:05:55 -0300

    Commit inicial

---
+++ src/main/java/me/lucasgsalmeida/gestao10x/Gestao10xApplication.java
@@ -0,0 +1,13 @@
+package me.lucasgsalmeida.gestao10x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gestao10xApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gestao10xApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/lucasgsalmeida/gestao10x/Gestao10xApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lucasgsalmeida.gestao10x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gestao10xApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
